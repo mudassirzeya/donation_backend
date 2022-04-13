6fbc9dbef003523d88986c66ab520b58ceb23682
mudassirzeya <78174941+mudassirzeya@users.noreply.github.com>
2022-04-13 21:51:58 +0530

    Update views.py

--- team/views.py
+++ team/views.py
@@ -1,4 +1,4 @@
-from tkinter import image_names
+# from tkinter import image_names
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
